d282a7175a70ffd90d2d873783809845bacc2831
Alex S <insten490@gmail.com>
2024-05-09 23:35:47 -0500

    Added key stuff and tested the app

--- react-movieapp/src/App.js
+++ react-movieapp/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       Start here.
     </div>
   );
 }
-
+  
 export default App;
--- react-movieapp/src/config.js
+++ react-movieapp/src/config.js
@@ -10,7 +10,7 @@ const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=en
 const REQUEST_TOKEN_URL = `${API_URL}authentication/token/new?api_key=${API_KEY}`;
 const LOGIN_URL = `${API_URL}authentication/token/validate_with_login?api_key=${API_KEY}`;
 const SESSION_ID_URL = `${API_URL}authentication/session/new?api_key=${API_KEY}`;
-
+import 
 const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
 // Sizes: w300, w780, w1280, original
 const BACKDROP_SIZE = 'w1280';
